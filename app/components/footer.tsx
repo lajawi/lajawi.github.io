64f75a0bfe39dc3dabfaae2bc534b3003d8e0548
lajawi <lajawi55@gmail.com>
2025-05-15 15:52:59 +0200

    Footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer className="flex flex-col justify-center items-center">
+            <hr className="border-[var(--line)] w-full" />
+            <div className="flex flex-col justify-center items-center max-w-[var(--max-w)] pt-4 pb-8 opacity-50 text-sm gap-2">
+                © 2025-present Portfolio | Laurin Winter
+            </div>
+        </footer>
+    );
+}

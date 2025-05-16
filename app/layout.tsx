5f67f92e5f8f9518c943c66f9e2ad3966120e167
lajawi <lajawi55@gmail.com>
2025-05-16 15:08:23 +0200

    Padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-full`}
       >
         <Header />
-        <main className="flex-1 flex flex-col items-center justify-center">
+        <main className="flex-1 flex flex-col items-center justify-center p-4">
           {children}
         </main>
         <Footer />

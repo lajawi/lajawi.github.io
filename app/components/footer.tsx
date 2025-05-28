cc2f38205d4b1ce242267a4193b1e0f2b3398370
lajawi <lajawi55@gmail.com>
2025-05-28 22:38:47 +0200

    Top Margin Footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@ import SocialLinks from "./socials";
 
 export default function Footer() {
     return (
-        <footer className="flex flex-col justify-center items-center text-[var(--muted)]">
+        <footer className="flex flex-col justify-center items-center text-[var(--muted)] mt-8">
             <hr className="border-[var(--line)] w-full" />
             <div className="flex flex-col justify-center items-center pt-4 pb-8 gap-2">
                 <SocialLinks />

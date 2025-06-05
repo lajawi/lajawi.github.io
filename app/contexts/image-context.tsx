05506f947f5a48b61019321b7be4c9407ff5317e
lajawi <lajawi55@gmail.com>
2025-06-05 23:18:14 +0200

    Debugging cleanup

--- app/contexts/image-context.tsx
+++ app/contexts/image-context.tsx
@@ -46,46 +46,33 @@ export const ImageProvider: React.FC<ImageProviderProps> = ({ children }) => {
     const [currentPath, setCurrentPath] = useState(pathname);
 
     if (currentPath !== pathname) {
-        console.log(`Pathname changed from ${currentPath} to ${pathname} - resetting images`);
         setImages([]);
-        console.log("Reset images due to pathname change");
         setCurrentIndex(-1);
-        console.log("Set current index to -1 due to pathname change");
         setIsFullscreen(false);
-        console.log("Set fullscreen to false due to pathname change");
         setCurrentPath(pathname);
-        console.log(`Set current pathname to ${pathname}`);
     }
 
     const registerImage = useCallback((image: Image) => {
         setImages((prevImages) => {
             if (!prevImages.some((img) => img.id === image.id)) {
-                console.log(`Registering image with id '${image.id}'`);
                 return [...prevImages, image];
             }
-            console.log(`Registering image with id '${image.id}' skipped because was already in the list`);
             return prevImages;
         });
     }, []);
 
     const openFullscreen = (id: string) => {
-        document.body.classList.add("overflow-y-hidden");
         const index = images.findIndex((img) => img.id === id);
         if (index !== -1) {
+            document.body.classList.add("overflow-y-hidden");
             setCurrentIndex(index);
-            console.log(`Setting current index to ${index}`);
             setIsFullscreen(true);
-            console.log("Fullscreen set to true");
-        } else {
-            console.log("Index is -1, not going fullscreen");
-            document.body.classList.remove("overflow-y-hidden");
         }
     };
 
     const closeFullscreen = () => {
         document.body.classList.remove("overflow-y-hidden");
         setIsFullscreen(false);
-        console.log("Set fullscreen to false");
     };
 
     const goToNext = () => {

4546bfd45fdb3f514943a639f81bab6259a94902
lajawi <lajawi55@gmail.com>
2025-06-05 21:15:57 +0200

    GitHub Pages changes & actions

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,4 +1,13 @@
 import { notFound } from "next/navigation";
+import { projects } from "../projects";
+
+export async function generateStaticParams() {
+    const projs = Object.entries(projects).filter((proj) => { return proj[0] !== 'team-tumble' && proj[0] !== 'bubble-broth' })
+
+    return projs.map((project) => ({
+        slug: project[0],
+    }));
+}
 
 export default function Page() {
     notFound();

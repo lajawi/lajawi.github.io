dec2b5c4098cdd9ac6980d4b6d5c41c9930e90c1
lajawi <lajawi55@gmail.com>
2025-06-20 16:19:12 +0200

    Opengraph Projects images

--- app/projects/opengraph-image.tsx
+++ app/projects/opengraph-image.tsx
@@ -1,4 +1,11 @@
 import { ImageResponse } from "next/og";
+import { sortedProjects } from "./projects";
+
+const n = 5;
+const projectsIds = sortedProjects('rank').slice(0, n).map((proj) => {
+    if (proj.data.image)
+        return proj.id;
+});
 
 export const size = {
   width: 1200,
@@ -9,7 +16,7 @@ export default async function Image() {
     return new ImageResponse((
         <div
             style={{
-                fontSize: 128,
+                fontSize: 96,
                 background: 'black',
                 color: 'white',
                 width: '100%',
@@ -19,7 +26,32 @@ export default async function Image() {
                 justifyContent: 'center',
             }}
         >
-            Projects • Laurin Winter
+            {projectsIds.map((id) => {
+                return <img
+                    style={{
+                        objectFit: 'cover',
+                        // transform: [{ skewX: '5deg' }],
+                    }}
+                    width={size.width / projectsIds.length}
+                    height={size.height}
+                    src={`${process.env.NEXT_PUBLIC_BASE_URL}/projects/${id}/cover.png`}
+                    alt=""
+                    key={id}
+                />
+            })}
+
+            <div style={{
+                position: 'absolute',
+                fontSize: 96,
+                background: '#00000080',
+                color: 'white',
+                width: '100%',
+                height: '100%',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+                Projects • Laurin Winter
+            </div>
         </div>
     ), {
         ...size,

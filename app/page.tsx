55434169b8e340eea873df7a3d47187cb641c2f7
lajawi <lajawi55@gmail.com>
2025-10-22 11:57:06 +0200

    "I am Laurin" doesn't jump around as much

--- app/page.tsx
+++ app/page.tsx
@@ -12,35 +12,31 @@ export default function Home() {
     <div className="w-(--page-width) justify-center flex flex-col align-middle items-center">
       <div className="rotating-text-demo flex-col sm:flex-row">
         <Image src={"/icon.svg"} alt="" width={200} height={200} className="m-8" />
-        <LayoutGroup>
-          <motion.p className="text-4xl sm:text-5xl md:text-6xl font-black font-mono rotating-text-ptag" layout>
-            <motion.span
-              className="pt-0.5 sm:pt-1 md:pt-2 text-(--foreground)"
-              layout
-              transition={{ type: "spring", damping: 30, stiffness: 400 }}
-            >
-              I am Laurin
-            </motion.span>
-            <RotatingText
-              texts={[
-                "Game Designer",
-                "Bachelor graduate",
-                "Programmer",
-                "Master student"
-              ]}
-              mainClassName="px-2 sm:px-2 md:px-3 bg-(--logo-cyan) text-(--background) overflow-hidden py-0.5 sm:py-1 md:py-2 rounded-lg text-nowrap"
-              staggerFrom={"first"}
-              initial={{ y: "100%" }}
-              animate={{ y: 0 }}
-              exit={{ y: "-120%" }}
-              staggerDuration={0.1}
-              splitBy="words"
-              splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-              transition={{ type: "spring", damping: 30, stiffness: 400 }}
-              rotationInterval={2000}
-            />
-          </motion.p>
-        </LayoutGroup>
+        <div className="text-4xl md:text-6xl font-black font-mono flex flex-col">
+          <p className="text-center sm:text-left">I am Laurin</p>
+          <LayoutGroup>
+            <motion.p className="rotating-text-ptag" layout>
+              <RotatingText
+                texts={[
+                  "Game Designer",
+                  "Bachelor graduate",
+                  "Programmer",
+                  "Master student"
+                ]}
+                mainClassName="px-2 sm:px-2 md:px-3 bg-(--logo-cyan) text-(--background) overflow-hidden py-0.5 sm:py-1 md:py-2 rounded-lg text-nowrap"
+                staggerFrom={"first"}
+                initial={{ y: "100%" }}
+                animate={{ y: 0 }}
+                exit={{ y: "-120%" }}
+                staggerDuration={0.1}
+                splitBy="words"
+                splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                transition={{ type: "spring", damping: 30, stiffness: 400 }}
+                rotationInterval={2000}
+              />
+            </motion.p>
+          </LayoutGroup>
+        </div>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-1 gap-4 mt-4 mb-4">
